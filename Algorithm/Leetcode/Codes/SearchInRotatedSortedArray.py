d760b9f174682f6918561da798afd5ae117143c1
Chloe Jungah Kim <hiyaku0317@gmail.com>
2020-03-11 23:53:03 +0900

    Update SearchInRotatedSortedArray.py

--- Algorithm/Leetcode/Codes/SearchInRotatedSortedArray.py
+++ Algorithm/Leetcode/Codes/SearchInRotatedSortedArray.py
@@ -15,12 +15,34 @@ Example:
 - Output : -1
 
 Note:
-nums[0] 값을 target과 비교하여 앞에서부터, 혹은 뒤에서부터 탐색할 지 결정하는 방법 가능
+- Solution 1
+python 내장함수 사용
+참고)nums[0] 값을 target과 비교하여 앞에서부터, 혹은 뒤에서부터 탐색할 지 결정하는 방법 가능
+- Solution 2
+Search in Rotated Sorted Array II와 동일한 방법
+target과 동일한 값이 있는 경우 해당 인덱스를 리턴
+리스트가 정렬되어 있기 때문에 이전 값이 target보다 작은데 다음 값이 target보다 큰 경우 target은 존재하지 않는다
 
 """
+
+# Solution 1
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if nums.count(target) == 0 :
             return -1
         return nums.index(target)
+
+# Solution 2
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        issmall = False
+        for i in range(len(nums)) :
+            if nums[i] == target :
+                return i
+            elif nums[i] < target :
+                issmall = True
+            elif nums[i] > target and issmall :
+                return -1
+        return -1
