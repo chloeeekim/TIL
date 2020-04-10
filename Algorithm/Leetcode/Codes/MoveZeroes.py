f8de1acf478ef9fd45696053b0442bcc20f3fc35
Chloe Jungah Kim <hiyaku0317@gmail.com>
2020-04-11 01:29:28 +0900

    Update MoveZeroes.py

--- Algorithm/Leetcode/Codes/MoveZeroes.py
+++ Algorithm/Leetcode/Codes/MoveZeroes.py
@@ -11,10 +11,16 @@ Example:
 - Output : [1,3,12,0,0]
 
 Note:
+- Solution 1
 리스트를 확인하면서 0이 나타나면 해당 원소를 지우고 뒤에 0을 append하는 방식
 리스트의 뒷부분에 0이 붙게 되므로, 몇 개의 0이 나타났는지를 확인하여 반복문 종료
+- Solution 2
+리스트에 추가/삭제하는 방식은 효율적이지 않으므로,
+0이 아닌 숫자를 앞으로 옮긴 이후 남은 리스트를 전부 0으로 만들어버리는 방식
 
 """
+
+# Solution 1
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
@@ -30,3 +36,18 @@ class Solution:
                 zero_count += 1
             else :
                 i += 1
+
+# Solution 2
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nidx = 0
+        for num in nums:
+            if num:
+                nums[nidx] = num
+                nidx += 1
+        for i in range(nidx, len(nums)):
+            nums[i] = 0
