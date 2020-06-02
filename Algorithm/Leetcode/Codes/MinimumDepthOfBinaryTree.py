2f7f3f4750c131ac33da62caa57ef244cf31eece
Chloe Jungah Kim <hiyaku0317@gmail.com>
2020-06-03 01:36:47 +0900

    Update MinimumDepthOfBinaryTree.py

--- Algorithm/Leetcode/Codes/MinimumDepthOfBinaryTree.py
+++ Algorithm/Leetcode/Codes/MinimumDepthOfBinaryTree.py
@@ -12,11 +12,17 @@ Example:
 - Output : 2
 
 Note:
+- Solution 1
 queue를 사용하여 level order로 순회
 level(depth)를 [node, level]의 형태로 queue에서 관리
+- Solution 2
+recursive하게 해결
+한 단계씩 들어갈 때마다 depth 값을 1씩 증가
+minimum 값을 구해야 하므로 sys.maxsize 값과 비교
 
 """
 
+# Solution 1
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -42,3 +48,24 @@ class Solution:
             if node.right :
                 queue.append([node.right, level + 1])
         return res
+
+# Solution 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root :
+            return 0
+        if not root.left and not root.right:
+            return 1
+        depth = sys.maxsize
+        if root.left:
+            depth = self.minDepth(root.left)
+        if root.right:
+            depth = min(depth, self.minDepth(root.right))
+        return depth+1
