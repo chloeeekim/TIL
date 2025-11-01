777c8a3ec84b6625f8ef278e1095b12709fa41a7
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-11-02 01:18:16 +0900

    Problem Solving: 개인정보 수집 유효기간 (kotlin)

---
+++ Algorithm/Programmers/Kotlin/150370.kt
@@ -0,0 +1,56 @@
+/*
+
+개인정보 수집 유효기간 : https://school.programmers.co.kr/learn/courses/30/lessons/150370
+
+개인정보와 약관이 주어졌을 때, 주어진 날짜를 기준으로 파기해야 할 개인정보 번호들을 구하는 문제
+- 모든 달은 28일까지 있다고 가정한다
+- today는 "YYY.MM.DD"의 형태로 주어진다
+- terms의 원소는 "약관종류 유효기간" 형태로, 공백 하나로 구분된다
+- 약관 종류는 A ~ Z 중 알파벳 대문자 하나이며, terms 배열에서 약관 종류는 중복되지 않는다
+- 유효기간은 개인정보를 보관할 수 있는 달 수를 나타내는 정수이며, 1 이상 100 이하이다
+- privacies[i]는 i+1번 개인정보의 수집 일자와 약관 종류를 나타낸다
+- privacies의 원소는 "날짜 약관종류" 형태로, 공백 하나로 구분된다
+- 날짜는 "YYYY.MM.DD" 형태이며, today 이전의 날짜만 주어진다
+- 모든 날짜는 "YYYY.MM.DD" 형태로 점(.) 하나로 구분되며, MM이나 DD가 한 자릿수인 경우 앞에 0이 붙는다
+- 파기해야 할 개인정보가 하나 이상 존재하는 입력만 주어진다
+
+Example:
+- Input : today="2022.05.19", terms=["A 6", "B 12", "C 3"], privacies=["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+- Output : [1, 3]
+- 첫 번째 개인정보는 2021년 11월 1일까지 보관 가능하므로 파기
+- 두 번째 개인정보는 2022년 6월 28일까지 보관 가능하므로 유지
+- 세 번째 개인정보는 2022년 5월 18일까지 보관 가능하므로 파기
+- 네 번째 개인정보는 2022년 5월 19일까지 보관 가능하므로 유지
+
+- Input : today="2020.01.01", terms=["Z 3", "D 5"], privacies=["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]
+- Output : [1,4,5]
+
+Note:
+YYYY.MM.DD 형태의 문자열을 2000년 1월 1일을 기준으로 몇 일째인지를 계산
+개인정보 수집 일자 + 유효기간 * 28일을 today와 비교
+mapIndexedNotNull을 사용하여 결과가 null이 아닌 요소만 리스트에 포함시킨 다음, IntArray로 변환
+
+ */
+
+class Solution {
+    fun solution(today: String, terms: Array<String>, privacies: Array<String>): IntArray {
+        val termMap = terms.associate { term ->
+            val (kind, duration) = term.split(" ")
+            kind to duration.toInt()
+        }
+
+        val todayDays = changeDays(today)
+
+        return privacies.mapIndexedNotNull { idx, privacy ->
+            val (date, term) = privacy.split(" ")
+            val expireDays = changeDays(date) + termMap[term]!! * 28
+
+            if (expireDays <= todayDays) idx + 1 else null
+        }.toIntArray()
+    }
+
+    private fun changeDays(day: String): Int {
+        val (yy, mm, dd) = day.split(".").map { it.toInt() }
+        return (yy - 2000) * 12 * 28 + (mm - 1) * 28 + dd
+    }
+}
